1388b9e194b0a8a82fd116380b535ad316a34404
yylev <93338153+yisraelevine@users.noreply.github.com>
2023-05-18 16:51:17 -0400

    edit

--- 6/script.js
+++ 6/script.js
@@ -1,17 +1,19 @@
 const sheetId = '1MNlbLGJt2NBiXabHWXWSpcHnv9nahq-LBePTOgi00OY';
 const urlSheets = `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq`;
-
 const containerForContainers = document.querySelector('#main>.container.a>.container.listcontainers');
 const containerForButtons = containerForContainers.querySelector('.container.liststories');
 const button = containerForButtons.querySelector('.button.story');
+
 containerForContainers.querySelector('.container.liststories').remove();
 containerForButtons.innerHTML = '';
 
+let containersToAdd;
 fetch(urlSheets).then(res => res.text()).then(rep => {
     const jsData = JSON.parse(rep.substring(47).slice(0, -2));
     const jsDataRows = jsData.table.rows;
     const jsDataRowsLen = jsDataRows.length;
-    const containersToAdd = Number((jsDataRowsLen / 100).toFixed());
+    
+    containersToAdd = Number((jsDataRowsLen / 100).toFixed());
 
     for (let i = 0, z = 0; i < containersToAdd; i++) {
         let buttonsToAdd;
@@ -21,6 +23,9 @@ fetch(urlSheets).then(res => res.text()).then(rep => {
             buttonsToAdd = 100;
         }
         const clone1 = containerForButtons.cloneNode(true);
+        if(i === 0){
+            clone1.style.display = 'block';
+        }
         for (let x = 0; x < buttonsToAdd; x++, z++) {
             const clone2 = button.cloneNode(true);
             clone2.innerHTML = jsDataRows[z].c[0].v;
@@ -28,5 +33,17 @@ fetch(urlSheets).then(res => res.text()).then(rep => {
         }
         containerForContainers.appendChild(clone1);
     }
-
 });
+
+let containerVisible = 0;
+function showOnlySpicificContainer(pos) {
+    containerForContainers.children[containerVisible].style.display = 'none';
+    if(pos === true && containerVisible < containersToAdd - 1){
+        containerVisible++;
+    } else if(containerVisible > 0) {
+        containerVisible--;
+    }
+    console.log(containerVisible);  
+    containerForContainers.children[containerVisible].style.display = 'block';
+}
+
