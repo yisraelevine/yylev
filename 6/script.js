3bc3d0269bf59717d82b4f75b4bcc00c2a0d4cf0
yylev <93338153+yisraelevine@users.noreply.github.com>
2023-05-18 14:44:35 -0400

    Update script.js

--- 6/script.js
+++ 6/script.js
@@ -1,20 +1,23 @@
-const numOfStories = 700;
-const container = document.querySelector('#main>.container.a>.container.listcontainers>.container.liststories');
-const node = container.querySelector('.button.story');
-container.querySelector('.button.story').remove();
-for(let i = 0; i < 100; i++){
-    const clone = node.cloneNode(true);
-    clone.innerHTML = i;
-    container.appendChild(clone);
-}
-
 const sheetId = '1MNlbLGJt2NBiXabHWXWSpcHnv9nahq-LBePTOgi00OY';
 const urlSheets = `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq`;
 
-let aaa;
+const containerForContainers = document.querySelector('#main>.container.a>.container.listcontainers');
+const containerForButtons = containerForContainers.querySelector('.container.liststories');
+const button = container.querySelector('.button.story');
+container.querySelector('.button.story').remove();
+
 fetch(urlSheets).then(res => res.text()).then(rep => {
     const jsData = JSON.parse(rep.substring(47).slice(0, -2));
     const jsDataRows = jsData.table.rows;
-    aaa = jsDataRows;
     const jsDataRowsLen = jsDataRows.length;
+    jsDataRows[0].c[0].v;
+    const containersToAdd = (jsDataRowsLen / 100).toFixed();
+    for(let i = 0; i < containersToAdd; i++){
+        
+    }
+        for(let i = 0; i < 100; i++){
+            const clone = node.cloneNode(true);
+            clone.innerHTML = i;
+            container.appendChild(clone);
+        }
 });
