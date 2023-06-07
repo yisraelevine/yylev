91a048aa86f9cf419aa9fa832604b4a93ca0035a
yylev <93338153+yisraelevine@users.noreply.github.com>
2023-06-07 18:35:30 -0400

    changing a bit the search operation

--- 10/indexFiles/scripts.js
+++ 10/indexFiles/scripts.js
@@ -6,10 +6,17 @@ inputBox.addEventListener('input', searchFiles);
 function searchFiles() {
     const inVal = this.value;
     filesNames.forEach(el => {
-        if (el.innerHTML.search(inVal) === -1) {
-            el.style.display = 'none';
+        const iT = el.innerText;
+        const i = iT.search(inVal);
+        const inValLength = inVal.length;
+        if (i === -1) {
+            //el.style.display = 'none';
+            el.innerHTML = iT;
+            el.parentElement.style.order = 0;
         } else {
-            el.style.display = 'inline-block';
+            el.parentElement.style.order = -1;
+            //el.style.display = 'inline-block';
+            el.innerHTML = iT.slice(0, i) + '<mark>' + iT.slice(i, i + inValLength) + '</mark>' + iT.slice(i + inValLength);
         }
     });
 }
