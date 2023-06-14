cb0b1211e6f3186edbd020845f3e91846b6ee694
yylev <93338153+yisraelevine@users.noreply.github.com>
2023-06-14 10:41:39 -0400

    developing 11

--- 11/indexFiles/scripts.js
+++ 11/indexFiles/scripts.js
@@ -1,2 +1,19 @@
 const sheetId = "1oz_UHSqdS7oszMsD42GgjBbgBy5sUn6tsMNo5qFPDy0";
 const urlSheets = `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq`;
+const main = document.querySelector('main');
+const content = main.querySelector('.content');
+
+function CIPOCBOO(){
+    if (content.scrollHeight > content.clientHeight) {
+        content.style.paddingLeft = '15px';
+    } else {
+        content.style.paddingLeft = 0;
+    }
+}
+setTimeout(function (){
+    CIPOCBOO();
+    main.style.opacity = 1;
+}, 100);
+window.addEventListener('resize', function () {
+    CIPOCBOO();
+});
