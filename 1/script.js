d2d4e0d4a9bccea5fd9af8f2e119ee007acb5220
yylev <93338153+yisraelevine@users.noreply.github.com>
2023-06-20 18:56:49 -0400

    optimizing 1 for performance and changes

--- 1/script.js
+++ 1/script.js
@@ -1,107 +1,53 @@
-const cor = document.getElementById("cor");
-const letch = document.getElementById("letch");
-const eifa = document.getElementById("eifa");
-const saha = document.getElementById("saha");
-const hin = document.getElementById("hin");
-const hisaron = document.getElementById("hisaron");
-const kab = document.getElementById("kab");
-const log = document.getElementById("log");
-const rviait = document.getElementById("rviait");
-const egg = document.getElementById("egg");
-const zait = document.getElementById("zait");
-const mesora = document.getElementById("mesora");
-const kortov = document.getElementById("kortov");
-const cm = document.getElementById("cm");
+const array = [
+    ['cor', 1, 'כור'],
+    ['letch', 2, 'לתך'],
+    ['eifa', 10, 'איפה'],
+    ['saha', 30, 'סאה'],
+    ['hin', 60, 'תרקב - הין'],
+    ['hisaron', 100, 'עשרון'],
+    ['kab', 180, 'קב'],
+    ['log', 720, 'לוג'],
+    ['rviait', 2880, 'רביעית'],
+    ['egg', 4320, 'ביצה'],
+    ['zait', 8640, 'כזית'],
+    ['mesora', 25920, 'משורה'],
+    ['kortov', 46080, 'קורטוב'],
+    ['cm', 248832, 'ס"מ מעוקב']
+]
 
-document.querySelectorAll('input').forEach(el => {
-    el.setAttribute('min', 0);
-    el.setAttribute('type', 'number');
+const main = document.querySelector('main');
+
+const nodeDiv = document.createElement('div');
+const nodeLabel = document.createElement('label');
+const nodeInput = document.createElement('input');
+
+nodeInput.setAttribute('min', 0);
+nodeInput.setAttribute('type', 'number');
+
+array.forEach(el => {
+    const cloneDiv = nodeDiv.cloneNode(true);
+    const cloneLabel = nodeLabel.cloneNode(true);
+    const cloneInput = nodeInput.cloneNode(true);
+
+    cloneLabel.setAttribute('for', el[0]);
+    cloneLabel.innerHTML = el[2];
+
+    cloneInput.id = el[0];
+    cloneInput.dataset.id = el[1];
+    cloneInput.addEventListener('input', calculator);
+
+    cloneDiv.appendChild(cloneLabel);
+    cloneDiv.appendChild(cloneInput);
+    main.appendChild(cloneDiv);
 });
 
-function calculator(num, id) {
-    num = parseFloat(num);
-    if (id !== 1) {
-        cor.value = (num * 1).toFixed(2);
-    }
-    if (id !== 2) {
-        letch.value = (num * 2).toFixed(2);
-    }
-    if (id !== 3) {
-        eifa.value = (num * 10).toFixed(2);
-    }
-    if (id !== 4) {
-        saha.value = (num * 30).toFixed(2);
-    }
-    if (id !== 5) {
-        hin.value = (num * 60).toFixed(2);
-    }
-    if (id !== 6) {
-        hisaron.value = (num * 100).toFixed(2);
-    }
-    if (id !== 7) {
-        kab.value = (num * 180).toFixed(2);
-    }
-    if (id !== 8) {
-        log.value = (num * 720).toFixed(2);
-    }
-    if (id !== 9) {
-        rviait.value = (num * 2880).toFixed(2);
-    }
-    if (id !== 10) {
-        egg.value = (num * 4320).toFixed(2);
-    }
-    if (id !== 11) {
-        zait.value = (num * 8640).toFixed(2);
-    }
-    if (id !== 12) {
-        mesora.value = (num * 25920).toFixed(2);
-    }
-    if (id !== 13) {
-        kortov.value = (num * 46080).toFixed(2);
-    }
-    if (id !== 14) {
-        cm.value = (num * 248832).toFixed(2);
-    }
-}
-function cor_action() {
-    calculator(cor.value / 1, 1);
-}
-function letch_action() {
-    calculator(letch.value / 2, 2);
-}
-function eifa_action() {
-    calculator(eifa.value / 10, 3);
-}
-function saha_action() {
-    calculator(saha.value / 30, 4);
-}
-function hin_action() {
-    calculator(hin.value / 60, 5);
-}
-function hisaron_action() {
-    calculator(hisaron.value / 100, 6);
-}
-function kab_action() {
-    calculator(kab.value / 180, 7);
-}
-function log_action() {
-    calculator(log.value / 720, 8);
-}
-function rviait_action() {
-    calculator(rviait.value / 2880, 9);
-}
-function egg_action() {
-    calculator(egg.value / 4320, 10);
-}
-function zait_action() {
-    calculator(zait.value / 8640, 11);
-}
-function mesora_action() {
-    calculator(mesora.value / 25920, 12);
-}
-function kortov_action() {
-    calculator(kortov.value / 46080, 13);
-}
-function cm_action() {
-    calculator(cm.value / 248832, 14);
+const input = document.querySelectorAll('input');
+
+function calculator() {
+    const num = parseFloat(this.value) / this.dataset.id;
+    input.forEach(el => {
+        if (this !== el) {
+            el.value = (num * Number(el.dataset.id)).toFixed(2);
+        }
+    });
 }
